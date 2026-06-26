0019d16d4fbc1dd2171d8281958b43c8020d1b7c
kubasimov <kubasimov@gmail.com>
2026-06-26 20:38:34 +0200

    refactor(auth): napraw politykę haseł - popraw składnię

--- backend/utils/password.js
+++ backend/utils/password.js
@@ -1,13 +1,13 @@
-function enforcePasswordPolicy(value) {
-      if (typeof value !== 'string') {
-        throw new Error('Haslo musi byc tekstem');
-      }
-      if (value.length < 10) {
-        throw new Error('Haslo musi miec co najmniej 10 znakow');
-      }
-      if (!/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[\W_]).*$/.test(value)) {
-        throw new Error('Haslo musi zawierac mala i duza litere, cyfre oraz znak specjalny');
-      }
-    }
-
-    module.exports = { enforcePasswordPolicy };
+function enforcePasswordPolicy(value) {                                                      
+  if (typeof value !== 'string') {                                                           
+    throw new Error('Haslo musi byc tekstem');                                               
+  }                                                                                          
+  if (value.length < 10) {                                                                   
+    throw new Error('Haslo musi miec co najmniej 10 znakow');                                
+  }                                                                                          
+  if (!/^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[\W_]).*$/.test(value)) {                            
+    throw new Error('Haslo musi zawierac mala i duza litere, cyfre oraz znak specjalny');    
+  }                                                                                          
+}                                                                                            
+                                                                                             
+module.exports = { enforcePasswordPolicy };                                                  
